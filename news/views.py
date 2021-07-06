f6a1a7c019701229f1e6a4b02d2faff18aca4c22
sngina <sngina707@gmail.com>
2021-07-06 09:35:27 +0300

    add django-registration

--- news/views.py
+++ news/views.py
@@ -50,7 +50,8 @@ def news_today(request):
             print('valid')
     else:
             form = NewsLetterForm()
-            return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+            print(form)
+            return render(request, 'all-news/today-news.html', {"date": date,"news":news,"anything":form})
 
 def search_results(request):
 
